293743f3bf5555f7b545968237874a3ee32d53f4
Sebastián Iturra Valdés <iturra.sebastian@hotmail.com>
2019-10-02 19:17:37 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <!--<router-link to="/">Inicio</router-link> |-->
-      <router-link to="/postulant">Listado de Postulantes</router-link> |
+      <router-link to="/postulant">Listado de Postulantes 2019</router-link> |
       <router-link to="/postulant/create">Crear Postulación</router-link>
     </div>
     <router-view />
